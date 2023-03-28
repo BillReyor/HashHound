878c47857d3e4457011c35751f9b38c9f866866b
Bill Reyor <87031733+BillReyor@users.noreply.github.com>
2023-03-28 18:07:27 -0400

    Update hashhound.py

--- hashhound.py
+++ hashhound.py
@@ -31,27 +31,31 @@ def export_pdf_to_text(pdf_contents: bytes) -> str:
     return contents
 
 
+def save_contents_to_file(file_path: str, contents: str):
+    with open(file_path, 'w') as f:
+        f.write(contents)
+
+
 def find_hashes(contents: str) -> defaultdict:
     grouped_hashes = defaultdict(list)
     hash_regexes = {
-        'MD5': r'\b([a-fA-F0-9]{32})\b',
-        'SHA1': r'\b([a-fA-F0-9]{40})\b',
-        'SHA256': r'\b([a-fA-F0-9]{64})\b'
+        'MD5': r'\b[a-fA-F0-9]{32}\b',
+        'SHA1': r'\b[a-fA-F0-9]{40}\b',
+        'SHA256': r'\b[a-fA-F0-9]{64}\b'
     }
 
-    # Split contents into rows based on one or more carriage returns
     rows = re.split(r'[\r\n]+', contents)
-
-    # Split each row into two cells based on one or more spaces
-    for row in rows:
-        cells = re.split(r'\s+', row.strip())
-        if len(cells) == 2:
-            # Concatenate the two cells and search for hashes within the concatenated string
-            combined_cell = cells[0] + cells[1]
-            for hash_type, regex in hash_regexes.items():
-                found_hashes = re.findall(regex, combined_cell)
-                if found_hashes:
-                    grouped_hashes[hash_type].extend(found_hashes)
+    for i, row in enumerate(rows):
+        for hash_type, regex in hash_regexes.items():
+            found_hashes = re.findall(regex, row)
+            if found_hashes:
+                grouped_hashes[hash_type].extend(found_hashes)
+            elif "Sample" in row:
+                # If the line has "Sample", this means it's a header row, and we should skip it.
+                continue
+            else:
+                # If no hashes were found, we can assume that it's a malware family name and skip this row.
+                break
 
     return grouped_hashes
 
@@ -68,10 +72,14 @@ def write_hashes_to_file(output_file_path: str, input_file_path: str, hashes: de
 
 def main():
     input_file_path = get_file_path("Please provide a file path for input: ")
+    intermediate_file_path = "file.txt"
     output_file_path = get_file_path("Please provide a file path for output: ")
 
     contents = read_contents(input_file_path)
-    hashes = find_hashes(contents)
+    save_contents_to_file(intermediate_file_path, contents)
+
+    contents_from_file = read_contents(intermediate_file_path)
+    hashes = find_hashes(contents_from_file)
     write_hashes_to_file(output_file_path, input_file_path, hashes)
 
 
